ab614cbaad723bd2ebc56416d35a1ef0e6679b21
floregol <florence.robert-regol@mail.mcgil.ca>
2023-03-24 19:47:12 -0400

    running 10

--- src/experiment/synthetic_run_config.py
+++ src/experiment/synthetic_run_config.py
@@ -45,7 +45,7 @@ class SyntheticRunConfig(RunConfig):
             self.diffusion_steps = self.T
             self.batch_size = 1024
             self.encoding_dim = 9
-            self.max_iterations = 2000
+            self.max_iterations =3500 
             self.transformer_dim = 128
             self.input_dp_rate = 0.2
             self.transformer_heads = 8
@@ -53,6 +53,6 @@ class SyntheticRunConfig(RunConfig):
             self.transformer_blocks = 1
             self.transformer_local_heads = 4
             self.transformer_local_size = 64
-            self.alpha = 1.1
-            self.corrected_var = 0.5
+            self.alpha = 1
+            self.corrected_var = 1
         super().set_dependent_config()
